83cac98ea22964f7b123b45404a4a74df391a4f9
hayashi45 <88770257+hayashi45@users.noreply.github.com>
2024-08-28 02:46:30 +0900

    Add files via upload
    Signed-off-by: hayashi45 <88770257+hayashi45@users.noreply.github.com>

---
+++ asset/js/slider.js
@@ -0,0 +1,37 @@
+$(".slide-items").slick({
+  dots: true, 
+  initialSlide: 1,
+  infinite: true,
+  arrows: true, 
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        dots: false, 
+        appendDots: $('.arrow_Dots'),
+        nextArrow: '<button type="button" class="slick-next">Next</button>',
+      }
+    }
+  ]
+});
+
+
+$(document).ready(function(){
+  $(".slide-items-gallery").slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    slidesToShow: 4,
+    arrows: false, 
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
