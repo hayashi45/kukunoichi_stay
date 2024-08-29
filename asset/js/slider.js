9a60278bcc7a7328e4e1796ce5e57de35b0675e0
hayashi45 <88770257+hayashi45@users.noreply.github.com>
2024-08-29 17:09:12 +0900

    Add files via upload
    Signed-off-by: hayashi45 <88770257+hayashi45@users.noreply.github.com>

---
+++ asset/js/slider.js
@@ -0,0 +1,24 @@
+
+    
+  // ギャラリースライダー
+  $(".slide-items-gallery").slick({
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 5000,
+    slidesToShow: 4,
+    cssEase: "linear",
+    arrows: false, 
+    infinite: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });     
